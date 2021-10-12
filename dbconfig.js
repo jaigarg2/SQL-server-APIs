111b53841936702d137fe88b8aa853d138f0bc00
Mohit_Gupta <guptamohit1504@gmail.com>
2021-10-12 23:24:09 +0530

    updates

--- dbconfig.js
+++ dbconfig.js
@@ -1,15 +1,19 @@
 
-const config = {
-	user: '',
-	password: '',
-	server: '',
-	database: '',
-	options: {
-		trustedconnection: true,
-		enableArithAort: true,
-		instancename: ''
-	},
-	port: 
-}
+const mysql = require('mysql');
 
-module.exports = config;
+var mysqlConnection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'edu1234',
+	database: 'cityfilter',
+	multipleStatements: true
+});
+
+mysqlConnection.connect((err)=> {
+	if(!err)
+		console.log('Connection Established Successfully');
+	else
+		console.log('Connection Failed!'+ JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mysqlConnection;
